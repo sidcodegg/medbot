96f2378eeb83ef282ec4f4883002bba2f2580b6c
Siddhesh <siddhesh.sathe1023@gmail.com>
2018-05-11 17:41:15 +0530

    Naming Convention Change

--- medbot.py
+++ medbot.py
@@ -2,32 +2,32 @@ from fuzzywuzzy import fuzz
 questions = open("ques.txt", 'r+')
 answers = open("ans.txt",'r+')
 
-predic = []
+questn = []
 answer = []
 
 
 for u in questions: 
-        predic.append(u)
+        questn.append(u)
 for n in answers:
         answer.append(n)
         
 print("Hi, I am medbot, Ask me your medical queries")
 def query():
 	inputer = raw_input("")
-	for i in predic:
+	for i in questn:
 		r = fuzz.ratio(inputer,i)
 		pr = fuzz.partial_ratio(inputer,i)
 		tsr = fuzz.token_set_ratio(inputer,i)
 		if pr > 95:
-			index = predic.index(i)
+			index = questn.index(i)
 			print 'MedBot :' + ' '+ (answer [index])
 			query()
 		elif r > 80:
-			index = predic.index(i)
+			index = questn.index(i)
 			print 'MedBot :' + ' '+ (answer [index])
 			query()
 		elif tsr > 85:
-			index = predic.index(i)
+			index = questn.index(i)
 			print 'MedBot :' + ' '+ (answer [index])
 			query()
 query()
